cca25e3835fda2b291a6485c95d13907f9b7552e
yates <yatesj3@mail.uc.edu>
2023-09-18 21:42:33 -0400

    Final Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 14;
+        if (numToExamine % 2 == 0) {
+            System.out.println("Num is even");
+        } else {
+            System.out.println("Num is odd");
+        }
+    }
+}
