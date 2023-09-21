060426e370923d56404d85f751c6113be938c150
yates <yatesj3@mail.uc.edu>
2023-09-21 15:34:35 -0400

    Print the number entered

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int numToExamine = 14;
+        System.out.println("Num entered is: " + numToExamine);
         if (numToExamine % 2 == 0) {
             System.out.println("Num is even");
         } else {
